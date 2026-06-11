03999f0c7d56f007a85ad43267df01bfd5fe40d1
0x8008 <balu@xdapierdolnik.pl>
2026-06-11 11:45:52 +0200

    Fix device tree for Infinity-X Android 16 build
    - BoardConfig.mk: Fix DEVICE_MATRIX_FILE path (device/qcom/common -> hardware/qcom-caf/common)
    - BoardConfig.mk: Fix DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE path
    - BoardConfig.mk: Add TARGET_KERNEL_SOURCE pointing to kernel/xiaomi/sm8350
    - BoardConfig.mk: Use TARGET_KERNEL_CONFIG instead of KERNEL_DEFCONFIG
    - BoardConfig.mk: Remove BOARD_VENDOR_KERNEL_MODULES (handled by kernel.mk)
    - lahaina.mk: Fix bluetooth audio policy config path to hardware/qcom-caf/sm8350

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,11 +76,11 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 LOC_HIDL_VERSION := 4.0
 
 # HIDL
-DEVICE_MATRIX_FILE := device/qcom/common/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
-    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
     $(DEVICE_PATH)/configs/vintf/manifest.xml
@@ -104,8 +104,9 @@ KERNEL_FULL_LLVM := true
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8350
+TARGET_KERNEL_CONFIG := vendor/star-qgki_defconfig
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
-KERNEL_DEFCONFIG := vendor/star-qgki_defconfig
 
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
@@ -126,67 +127,6 @@ BOOT_KERNEL_MODULES := \
     hwid.ko
 
 BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
-
-BOARD_VENDOR_KERNEL_MODULES := \
-    $(KERNEL_MODULES_OUT)/bt_fm_slim.ko \
-    $(KERNEL_MODULES_OUT)/btpower.ko \
-    $(KERNEL_MODULES_OUT)/camera.ko \
-    $(KERNEL_MODULES_OUT)/cnss2.ko \
-    $(KERNEL_MODULES_OUT)/device_management_service_v01.ko \
-    $(KERNEL_MODULES_OUT)/e4000.ko \
-    $(KERNEL_MODULES_OUT)/fc0011.ko \
-    $(KERNEL_MODULES_OUT)/fc0012.ko \
-    $(KERNEL_MODULES_OUT)/fc0013.ko \
-    $(KERNEL_MODULES_OUT)/fc2580.ko \
-    $(KERNEL_MODULES_OUT)/goodix_fod.ko \
-    $(KERNEL_MODULES_OUT)/hid-aksys.ko \
-    $(KERNEL_MODULES_OUT)/hwid.ko \
-    $(KERNEL_MODULES_OUT)/icnss2.ko \
-    $(KERNEL_MODULES_OUT)/ir-spi.ko \
-    $(KERNEL_MODULES_OUT)/it913x.ko \
-    $(KERNEL_MODULES_OUT)/led-class-flash.ko \
-    $(KERNEL_MODULES_OUT)/leds-qti-flash.ko \
-    $(KERNEL_MODULES_OUT)/leds-qti-tri-led.ko \
-    $(KERNEL_MODULES_OUT)/ledtrig-timer.ko \
-    $(KERNEL_MODULES_OUT)/llcc_perfmon.ko \
-    $(KERNEL_MODULES_OUT)/m88rs6000t.ko \
-    $(KERNEL_MODULES_OUT)/max2165.ko \
-    $(KERNEL_MODULES_OUT)/mc44s803.ko \
-    $(KERNEL_MODULES_OUT)/mi_thermal_interface.ko \
-    $(KERNEL_MODULES_OUT)/msi001.ko \
-    $(KERNEL_MODULES_OUT)/mt2060.ko \
-    $(KERNEL_MODULES_OUT)/mt2063.ko \
-    $(KERNEL_MODULES_OUT)/mt20xx.ko \
-    $(KERNEL_MODULES_OUT)/mt2131.ko \
-    $(KERNEL_MODULES_OUT)/mt2266.ko \
-    $(KERNEL_MODULES_OUT)/mxl301rf.ko \
-    $(KERNEL_MODULES_OUT)/mxl5005s.ko \
-    $(KERNEL_MODULES_OUT)/mxl5007t.ko \
-    $(KERNEL_MODULES_OUT)/nfc_i2c.ko \
-    $(KERNEL_MODULES_OUT)/qm1d1b0004.ko \
-    $(KERNEL_MODULES_OUT)/qm1d1c0042.ko \
-    $(KERNEL_MODULES_OUT)/qt1010.ko \
-    $(KERNEL_MODULES_OUT)/qti_battery_charger_main.ko \
-    $(KERNEL_MODULES_OUT)/r820t.ko \
-    $(KERNEL_MODULES_OUT)/rdbg.ko \
-    $(KERNEL_MODULES_OUT)/si2157.ko \
-    $(KERNEL_MODULES_OUT)/slimbus.ko \
-    $(KERNEL_MODULES_OUT)/slimbus-ngd.ko \
-    $(KERNEL_MODULES_OUT)/stmvl53l5.ko \
-    $(KERNEL_MODULES_OUT)/tda18212.ko \
-    $(KERNEL_MODULES_OUT)/tda18218.ko \
-    $(KERNEL_MODULES_OUT)/tda18250.ko \
-    $(KERNEL_MODULES_OUT)/tda9887.ko \
-    $(KERNEL_MODULES_OUT)/tea5761.ko \
-    $(KERNEL_MODULES_OUT)/tea5767.ko \
-    $(KERNEL_MODULES_OUT)/tua9001.ko \
-    $(KERNEL_MODULES_OUT)/tuner-simple.ko \
-    $(KERNEL_MODULES_OUT)/tuner-types.ko \
-    $(KERNEL_MODULES_OUT)/tuner-xc2028.ko \
-    $(KERNEL_MODULES_OUT)/wlan_firmware_service_v01.ko \
-    $(KERNEL_MODULES_OUT)/wlan.ko \
-    $(KERNEL_MODULES_OUT)/xc4000.ko \
-    $(KERNEL_MODULES_OUT)/xc5000.ko
 
 # Lineage Health
 #TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
